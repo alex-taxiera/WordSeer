d55977875e37e72ffd64445b9524e4ea8babb8da
Reuben Kernan <kernan94@live.com>
2018-03-27 12:12:02 -0400

    Defining tests for synonym feature

---
+++ modules/commands/synonym.js
@@ -0,0 +1,11 @@
+const Command = require('./Command.js')
+
+module.exports = new Command({
+  name: 'synonym',
+  description: 'Find and display synonyms for a specific word',
+  parameters: ['word'],
+  permission: 'Anyone',
+  run: async ({ msg, params }) => {
+    return 'Not yet implemented'
+  }
+})
---
+++ test/synonym.js
@@ -0,0 +1,19 @@
+const assert = require('assert')
+const command = require('../commands')
+
+describe('Synonyms', function () {
+  it('should return error message if word doesn\'t exist', async function () {
+    assert.equal(await command.synonym.execute('sssssssnake'), 'Word does not exist')
+  })
+  it('should return embed object with synonym data', async function () {
+    let word = 'snake'
+    assert.equal(await command.synonym.execute(word), {
+      title: `Synonyms for: ${word}`,
+      description: 'beast, boor, cad, churl, clown, creep, cur, dog, heel, jerk, joker, louse, lout, pill, rat, reptile, scum, skunk, slob, stinker, swine, varmint, vermin'
+    })
+  })
+  // TODO: Error handling
+  // it('', async function () {
+  //   assert.equal(await command.l.execute('ssssssssssssnake'), '')
+  // })
+})
