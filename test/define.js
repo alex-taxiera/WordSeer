061c39910f1f80d1cd86552eae7dcf804df21e96
kernan94 <reuben.kernan@gmail.com>
2018-03-30 00:01:32 -0400

    localizes data to secific tests

--- test/define.js
+++ test/define.js
@@ -1,20 +1,6 @@
 const assert = require('assert')
 const command = require('../modules/commands')
 
-const author = {
-  id: '123',
-  mention: ''
-}
-const channel = {
-  id: '123',
-  messages: []
-}
-const msg = {
-  id: '2',
-  author,
-  channel
-}
-
 const suggestions = [
   'snake',
   'sneak',
@@ -40,12 +26,52 @@ const suggestions = [
 
 describe('Define', function () {
   it('should return error message if word does not exist', async function () {
+    const author = {
+      id: '123',
+      mention: ''
+    }
+    const channel = {
+      id: '123',
+      messages: []
+    }
+    const msg = {
+      id: '2',
+      author,
+      channel
+    }
     assert.equal(await command.define.run({ msg, params: ['sssssssnake'] }), 'Word does not exist')
   })
   it('should return suggestions if word is similar to, but not actually a word', async function () {
+    const author = {
+      id: '123',
+      mention: ''
+    }
+    const channel = {
+      id: '123',
+      messages: []
+    }
+    const msg = {
+      id: '2',
+      author,
+      channel
+    }
     assert.equal(await command.define.run({ msg, params: ['ssnake'] }), `Word does not exist, try ${suggestions.join(', ')}`)
   })
   it('should return embed object with definition(s), functional label, popularity, and pronunciation', async function () {
+    const author = {
+      id: '123',
+      mention: ''
+    }
+    const channel = {
+      id: '123',
+      messages: []
+    }
+    channel.messages.push({ id: '3', author, channel, content: '1' })
+    const msg = {
+      id: '2',
+      author,
+      channel
+    }
     const params = ['snake']
     const expected = {
       content: '',
@@ -72,13 +98,33 @@ describe('Define', function () {
         ]
       }
     }
-    await channel.messages.push({ id: '3', content: '1', author })
     command.define.run({ msg, params }).then((actual) => {
       assert.deepEqual(actual, expected)
+      // assert.equal(actual.content, expected.content)
+      // assert.equal(actual.embed.description, expected.embed.description)
+      // assert.equal(actual.embed.title, expected.embed.title)
+      // for (let i = 0; i < expected.embed.fields.length; i++) {
+      //   console.log('actual: ', actual.embed.fields[i].value)
+      //   console.log('expected: ', expected.embed.fields[i].value)
+      //   assert.equal(actual.embed.fields[i].name, expected.embed.fields[i].name)
+      //   assert.equal(actual.embed.fields[i].value, expected.embed.fields[i].value)
+      // }
     })
   })
   it('should return undefined if user does not respond to specify message', async function () {
-    channel.messages = []
+    const author = {
+      id: '123',
+      mention: ''
+    }
+    const channel = {
+      id: '123',
+      messages: []
+    }
+    const msg = {
+      id: '2',
+      author,
+      channel
+    }
     channel.messages.push(msg)
     assert.equal(await command.define.run({ msg, params: ['snake'] }), undefined)
   })
