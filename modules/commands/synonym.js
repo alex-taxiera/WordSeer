577daccb913ae5d92768eb737a099c6cfa26f7c3
Alex Taxiera <alex.taxiera@gmail.com>
2018-03-29 23:05:54 -0400

    properly resolve/reject promise

--- modules/commands/synonym.js
+++ modules/commands/synonym.js
@@ -8,77 +8,27 @@ module.exports = new Command({
   permission: 'Anyone',
   run: async ({ msg, params }) => {
     let word = params.join(' ')
-    let promise = new Promise()
-    let results = await mw.search(word) // returns a list of words, object { suggestions: [] }, or undefined
-    if (!results) promise.resolve('Word does not exist')
-    if (results.suggestions) promise.resolve(`Word does not exist, try ${results.suggestions.join(', ')}`)
-    if (results.length === 1) {
-      let synonyms = []
-
-      for (let i = 0; i < results[0].definition.length; i++) {
-        let definition = results[0].definition[i]
-        if (definition.senses) {
-          for (let j = 0; j < definition.senses.length; j++) {
-            if (definition.senses[j].synonyms) synonyms = synonyms.concat(definition.senses[j].synonyms)
-          }
-        } else if (definition.synonyms) synonyms = synonyms.concat(definition.synonyms)
-      }
-      if (synonyms.length === 0) {
-        promise.resolve(`No synonyms for ${word}`)
+    return new Promise(async (resolve, reject) => {
+      let results = await mw.search(word) // returns a list of words, object { suggestions: [] }, or undefined
+      if (!results) {
+        resolve('Word does not exist')
       } else {
-        promise.resolve({
-          content: '',
-          embed: {
-            title: `Synonyms for: ${results[0].word}`,
-            description: synonyms.join(', ')
+        if (results.suggestions) resolve(`Word does not exist, try ${results.suggestions.join(', ')}`)
+        if (results.length === 1) {
+          let synonyms = []
+          for (let i = 0; i < results[0].definition.length; i++) {
+            let definition = results[0].definition[i]
+            if (definition.senses) {
+              for (let j = 0; j < definition.senses.length; j++) {
+                if (definition.senses[j].synonyms) synonyms = synonyms.concat(definition.senses[j].synonyms)
+              }
+            } else if (definition.synonyms) synonyms = synonyms.concat(definition.synonyms)
           }
-        })
-      }
-    }
-    if (results.length > 1) {
-      results = results.filter((val, i) => i < 10)
-      let embed = {
-        title: 'What we found:',
-        fields: []
-      }
-      for (let i = 0; i < results.length; i++) {
-        const entry = results[0]
-        embed.fields.push({
-          name: entry.word,
-          value: `Function: ${entry.functional_label}`
-        })
-        if (entry.etymology) embed.fields[i].value = embed.fields[i].value + `\nEtymology: ${entry.etymology}`
-      }
-      console.log({content: `${msg.author.mention} Select a number 1-${results.length + 1}`, embed})
-      // const specify = await msg.channel.createMessage({
-      //   content: `${msg.author.mention} Select a number 1-${results.length + 1}`,
-      //   embed
-      // })
-      let count = 0
-      let loop = setInterval(function () {
-        count++
-        let last = msg.channel.messages.filter((m) => m.author.id === msg.author.id)
-        last = last[last.length - 1]
-        if (last.id !== msg.id) {
-          let response = last.content
-          if (isNaN(response) || response < 1 || response > results.length + 1) {
-            clearInterval(loop)
-            // specify.delete()
-            promise.reject('Bad response')
+          console.log('1', synonyms)
+          if (synonyms.length === 0) {
+            resolve(`No synonyms for ${word}`)
           } else {
-            clearInterval(loop)
-            // specify.delete()
-            const final = results[response - 1]
-            let synonyms = []
-            for (let i = 0; i < final.definition.length; i++) {
-              let definition = final.definition[i]
-              if (definition.senses) {
-                for (let j = 0; j < definition.senses.length; j++) {
-                  if (definition.senses[j].synonyms) synonyms = synonyms.concat(definition.senses[j].synonyms)
-                }
-              } else if (definition.synonyms) synonyms = synonyms.concat(definition.synonyms)
-            }
-            promise.resolve({
+            resolve({
               content: '',
               embed: {
                 title: `Synonyms for: ${results[0].word}`,
@@ -87,13 +37,70 @@ module.exports = new Command({
             })
           }
         }
-        if (count > 30) {
-          clearInterval(loop)
-          // specify.delete()
-          promise.reject('No response')
+        if (results.length > 1) {
+          results = results.filter((val, i) => i < 10)
+          let embed = {
+            title: 'What we found:',
+            fields: []
+          }
+          for (let i = 0; i < results.length; i++) {
+            const entry = results[0]
+            embed.fields.push({
+              name: entry.word,
+              value: `Function: ${entry.functional_label}`
+            })
+          }
+          // console.log({content: `${msg.author.mention} Select a number 1-${results.length + 1}`, embed})
+          // const specify = await msg.channel.createMessage({
+          //   content: `${msg.author.mention} Select a number 1-${results.length + 1}`,
+          //   embed
+          // })
+          let count = 0
+          let loop = setInterval(function () {
+            count++
+            let last = msg.channel.messages.filter((m) => m.author.id === msg.author.id)
+            last = last[last.length - 1]
+            // console.log(last)
+            if (last.id !== msg.id) {
+              let response = last.content
+              if (isNaN(response) || response < 1 || response > results.length + 1) {
+                clearInterval(loop)
+                // specify.delete()
+                resolve('Bad response')
+              } else {
+                clearInterval(loop)
+                // specify.delete()
+                const final = results[response - 1]
+                let synonyms = []
+                for (let i = 0; i < final.definition.length; i++) {
+                  let definition = final.definition[i]
+                  if (definition.senses) {
+                    for (let j = 0; j < definition.senses.length; j++) {
+                      if (definition.senses[j].synonyms) synonyms = synonyms.concat(definition.senses[j].synonyms)
+                    }
+                  } else if (definition.synonyms) synonyms = synonyms.concat(definition.synonyms)
+                }
+                if (synonyms.length === 0) {
+                  resolve(`No synonyms for ${word}`)
+                } else {
+                  resolve({
+                    content: '',
+                    embed: {
+                      title: `Synonyms for: ${results[0].word}`,
+                      description: synonyms.join(', ')
+                    }
+                  })
+                }
+              }
+            }
+            if (count > 5) {
+              clearInterval(loop)
+              // specify.delete()
+              resolve(undefined)
+            }
+          }, 200)
         }
-      }, 1000)
-    }
-    return promise
+      }
+    })
   }
 })
